3d992457300aa83dd7fba88e701d4bb60b035365
Keysean Amison <key@Keyseans-MacBook-Pro.local>
2025-03-08 15:51:46 -0500

    committing first round of files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Over Under project for MLB',
+    author='Keysean Amison',
+    license='MIT',
+)
